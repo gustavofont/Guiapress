bfa32de4c1dc4a7a01eef1fb16a58466660cef7a
gustavofont <gustavo-machado-fontenele@hotmail.com>
2021-12-15 09:09:58 -0300

    fix: categorias

--- cathegories/cathegory.js
+++ cathegories/cathegory.js
@@ -1,14 +1,15 @@
 const Sequelize = require("sequelize")
 const connection = require("../database/database")
 
-const Category = connection.define('cathegories',{
+const Cathegory = connection.define('cathegory',{
     title:{
-        type: Sequelize.STRING,
+        type : Sequelize.STRING,
         allowNull: false
     },slug:{
-        type: Sequelize.STRING,
-        allowNull: false
+        type : Sequelize.STRING,
+        allowNull : false
     }
 })
 
-module.exports = Category
+
+module.exports = Cathegory
